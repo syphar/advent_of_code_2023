2d4cef295b58329f0d093c1ba5d73a46fde789e4
Denis Cornehl <denis@cornehl.org>
2023-12-01 10:10:55 +0100

    d01: refactor with new method

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -5,27 +5,11 @@ fn main() {
         .map(|l| l.to_string())
         .collect();
 
-    println!("part 1: {}", part_1(lines.iter()));
-    println!("part 2: {}", part_2(lines.iter()));
+    println!("part 1: {}", run(lines.iter()));
+    println!("part 2: {}", run(lines.iter()));
 }
 
-fn part_1<T: AsRef<str>>(lines: impl Iterator<Item = T>) -> u64 {
-    lines
-        .filter_map(|line| {
-            let line = line.as_ref().trim();
-            let digits: Vec<u16> = line
-                .chars()
-                .filter(|c| c.is_ascii_digit())
-                .map(|c| c as u16 - '0' as u16)
-                .collect();
-
-            (!digits.is_empty())
-                .then(|| *digits.first().unwrap() as u64 * 10 + *digits.last().unwrap() as u64)
-        })
-        .sum()
-}
-
-fn part_2<T: AsRef<str>>(lines: impl Iterator<Item = T>) -> u64 {
+fn find_digit(line: &[u8], range: impl IntoIterator<Item = usize>) -> Option<u64> {
     static DIGITS: [(&[u8], u64); 10] = [
         (b"one", 1),
         (b"two", 2),
@@ -39,40 +23,27 @@ fn part_2<T: AsRef<str>>(lines: impl Iterator<Item = T>) -> u64 {
         (b"zero", 0),
     ];
 
+    for start in range {
+        if line[start].is_ascii_digit() {
+            return Some(line[start] as u64 - b'0' as u64);
+        }
+
+        for (value, digit) in DIGITS.iter() {
+            if line[start..].starts_with(value) {
+                return Some(*digit);
+            }
+        }
+    }
+    None
+}
+
+fn run<T: AsRef<str>>(lines: impl Iterator<Item = T>) -> u64 {
     lines
         .filter_map(|line| {
-            let line = line.as_ref().trim();
-            let line = line.as_bytes();
+            let line = line.as_ref().trim().as_bytes();
 
-            let mut first_digit: Option<u64> = None;
-            'outer: for start in 0usize..line.len() {
-                if line[start].is_ascii_digit() {
-                    first_digit = Some(line[start] as u64 - b'0' as u64);
-                    break 'outer;
-                }
-
-                for (value, digit) in DIGITS.iter() {
-                    if line[start..].starts_with(value) {
-                        first_digit = Some(*digit);
-                        break 'outer;
-                    }
-                }
-            }
-
-            let mut last_digit: Option<u64> = None;
-            'outer: for start in (0usize..line.len()).rev() {
-                if line[start].is_ascii_digit() {
-                    last_digit = Some(line[start] as u64 - b'0' as u64);
-                    break 'outer;
-                }
-
-                for (value, digit) in DIGITS.iter() {
-                    if line[start..].starts_with(value) {
-                        last_digit = Some(*digit);
-                        break 'outer;
-                    }
-                }
-            }
+            let first_digit = find_digit(line, 0usize..line.len());
+            let last_digit = find_digit(line, (0usize..line.len()).rev());
 
             Some(first_digit? * 10 + last_digit?)
         })
@@ -86,13 +57,11 @@ mod tests {
     #[test]
     fn test_1() {
         assert_eq!(
-            part_1(
-                "1abc2
+            run("1abc2
                  pqr3stu8vwx
                  a1b2c3d4e5f
                  treb7uchet"
-                    .lines()
-            ),
+                .lines()),
             142
         )
     }
@@ -100,8 +69,7 @@ mod tests {
     #[test]
     fn test_2() {
         assert_eq!(
-            part_2(
-                "
+            run("
                 two1nine
                 eightwothree
                 abcone2threexyz
@@ -109,8 +77,7 @@ mod tests {
                 4nineeightseven2
                 zoneight234
                 7pqrstsixteen"
-                    .lines()
-            ),
+                .lines()),
             281
         );
     }
