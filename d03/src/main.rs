013e041c16de9a90bdc1594dd391631ce5ba9546
Denis Cornehl <denis@cornehl.org>
2023-12-05 05:03:02 +0100

    d03/2 works but ugly

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = include_str!("../input.txt");
 
     println!("part 1: {}", part_1(input));
+    println!("part 2: {}", part_2(input));
 }
 
 fn contains_symbol(line: &str, from: i64, to: i64) -> bool {
@@ -66,9 +67,109 @@ fn part_1(input: &str) -> u64 {
     sum
 }
 
+fn fetch_number(line: &str, start: i64) -> Option<u64> {
+    println!("fetch_number: {} -> {}", start, line);
+    let start = start.max(0) as usize;
+
+    let Some(start_ch) = line.chars().nth(start) else {
+        return None;
+    };
+    if !start_ch.is_ascii_digit() {
+        return None;
+    }
+
+    let mut number = String::from(start_ch);
+
+    for idx in (0..start).rev() {
+        let ch = line.chars().nth(idx).unwrap();
+        if ch.is_ascii_digit() {
+            number.insert(0, ch);
+        } else {
+            break;
+        }
+    }
+
+    for idx in (start + 1)..line.len() {
+        if let Some(ch) = line.chars().nth(idx) {
+            if ch.is_ascii_digit() {
+                number.push(ch)
+            } else {
+                break;
+            }
+        }
+    }
+
+    Some(number.parse::<u64>().unwrap())
+}
+
+fn part_2(input: &str) -> u64 {
+    let lines: Vec<_> = input
+        .lines()
+        .filter(|l| !l.trim().is_empty())
+        .map(str::trim)
+        .map(|l| format!(".{}.", l))
+        .collect();
+
+    let mut sum = 0;
+
+    for y in 0..lines.len() {
+        let line = lines[y].to_owned();
+
+        for x in 0..line.len() {
+            let c = line.chars().nth(x).unwrap();
+            println!("X: {} -> {}", x, c);
+
+            if c == '*' {
+                let mut gears = Vec::new();
+                if let Some(number) = fetch_number(&line, x as i64 - 1) {
+                    gears.push(dbg!(number));
+                }
+
+                if let Some(number) = fetch_number(&line, x as i64 + 1) {
+                    gears.push(dbg!(number));
+                }
+
+                if let Some(number) = fetch_number(&lines[y - 1], x as i64) {
+                    gears.push(dbg!(number));
+                } else {
+                    if let Some(number) = fetch_number(&lines[y - 1], x as i64 - 1) {
+                        gears.push(dbg!(number));
+                    }
+                    if let Some(number) = fetch_number(&lines[y - 1], x as i64 + 1) {
+                        gears.push(dbg!(number));
+                    }
+                }
+
+                if let Some(number) = fetch_number(&lines[y + 1], x as i64) {
+                    gears.push(dbg!(number));
+                } else {
+                    if let Some(number) = fetch_number(&lines[y + 1], x as i64 - 1) {
+                        gears.push(dbg!(number));
+                    }
+                    if let Some(number) = fetch_number(&lines[y + 1], x as i64 + 1) {
+                        gears.push(dbg!(number));
+                    }
+                }
+
+                if gears.len() == 2 {
+                    println!("LINE: {}", lines[y - 1]);
+                    println!("LINE: {}", lines[y]);
+                    println!("LINE: {}", lines[y + 1]);
+                    println!("gears: {:?}", gears);
+                    let ratio = gears[0] * gears[1];
+                    sum += ratio;
+                }
+            }
+        }
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
     static TEST_INPUT: &str = "
         467..114..
@@ -88,8 +189,27 @@ mod tests {
         assert_eq!(part_1(TEST_INPUT), 4361);
     }
 
-    // #[test]
-    // fn test_2() {
-    //     assert_eq!(run_on_input(TEST_INPUT, part_2), 2286);
-    // }
+    #[test]
+    fn test_2() {
+        assert_eq!(part_2(TEST_INPUT), 467835);
+    }
+
+    #[test_case("..456..", -1, None; "test 1")]
+    #[test_case("..456..", 0, None)]
+    #[test_case("..456..", 1, None)]
+    #[test_case("..456..", 2, Some(456))]
+    #[test_case("..456..", 3, Some(456))]
+    #[test_case("..456..", 4, Some(456))]
+    #[test_case("..456..", 5, None)]
+    #[test_case("789..", -2, Some(789); "test2")]
+    #[test_case("789..", 0, Some(789))]
+    #[test_case("789..", 1, Some(789))]
+    #[test_case("789..", 2, Some(789))]
+    #[test_case("789..", 3, None)]
+    #[test_case("789..", 4, None)]
+    #[test_case("789..", 5, None)]
+    #[test_case("789..", 6, None)]
+    fn test_fetch_number(line: &str, start: i64, expected: Option<u64>) {
+        assert_eq!(fetch_number(line, start), expected);
+    }
 }
