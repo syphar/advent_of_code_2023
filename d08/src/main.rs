46741690eb975c41a8bc27c64c89aa8e8d68ecb3
Denis Cornehl <denis@cornehl.org>
2023-12-08 07:01:10 +0100

    08/2 might be correct, but runs long

--- d08/src/main.rs
+++ d08/src/main.rs
@@ -7,11 +7,13 @@ use regex::Regex;
 fn main() {
     let input = include_str!("../input.txt");
 
-    println!("part 1: {}", part_1(input));
-    // println!("part 2: {}", run(parse_races_2(input).iter().copied()));
+    let (directions, instructions) = parse(input);
+
+    println!("part 1: {}", part_1(&directions, &instructions));
+    println!("part 2: {}", part_2(&directions, &instructions));
 }
 
-fn part_1(input: &str) -> u64 {
+fn parse(input: &str) -> (Vec<Direction>, HashMap<String, (String, String)>) {
     let mut lines = input.lines().map(str::trim).filter(|line| !line.is_empty());
 
     let directions: Vec<_> = lines
@@ -24,32 +26,62 @@ fn part_1(input: &str) -> u64 {
 
     let pattern = Regex::new(r"(\w+) = \((\w+), (\w+)\)").unwrap();
 
-    let rules: HashMap<_, _> = lines
+    let instructions: HashMap<_, _> = lines
         .map(|line| {
             let captures = pattern.captures(line).unwrap();
             (
-                captures.get(1).unwrap().as_str(),
+                captures.get(1).unwrap().as_str().to_owned(),
                 (
-                    captures.get(2).unwrap().as_str(),
-                    captures.get(3).unwrap().as_str(),
+                    captures.get(2).unwrap().as_str().to_owned(),
+                    captures.get(3).unwrap().as_str().to_owned(),
                 ),
             )
         })
         .collect();
 
+    (directions, instructions)
+}
+
+fn part_1(directions: &[Direction], instructions: &HashMap<String, (String, String)>) -> u64 {
     let mut position = "AAA";
     let mut steps = 0;
     for direction in directions.iter().cycle() {
-        let next_move = rules.get(position).unwrap();
+        let next_move = instructions.get(position).unwrap();
 
         match direction {
-            Direction::Left => position = next_move.0,
-            Direction::Right => position = next_move.1,
+            Direction::Left => position = &next_move.0,
+            Direction::Right => position = &next_move.1,
         };
 
         steps += 1;
 
         if position == "ZZZ" {
+            return steps;
+        }
+    }
+    unreachable!();
+}
+
+fn part_2(directions: &[Direction], instructions: &HashMap<String, (String, String)>) -> u64 {
+    let mut positions: Vec<_> = instructions
+        .keys()
+        .filter(|key| key.ends_with('A'))
+        .collect();
+
+    let mut steps = 0;
+    for direction in directions.iter().cycle() {
+        for position in &mut positions {
+            let next_move = instructions.get(*position).unwrap();
+
+            match direction {
+                Direction::Left => *position = &next_move.0,
+                Direction::Right => *position = &next_move.1,
+            };
+        }
+
+        steps += 1;
+
+        if positions.iter().all(|position| position.ends_with('Z')) {
             return steps;
         }
     }
@@ -80,13 +112,34 @@ mod tests {
         ZZZ = (ZZZ, ZZZ)
         ";
 
+    static TEST_INPUT_3: &str = "
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+        ";
+
     #[test]
     fn test_1() {
-        assert_eq!(part_1(TEST_INPUT), 2);
+        let (directions, instructions) = parse(TEST_INPUT);
+        assert_eq!(part_1(&directions, &instructions), 2);
     }
 
     #[test]
     fn test_1_2() {
-        assert_eq!(part_1(TEST_INPUT_2), 6);
+        let (directions, instructions) = parse(TEST_INPUT_2);
+        assert_eq!(part_1(&directions, &instructions), 6);
+    }
+
+    #[test]
+    fn test_2() {
+        let (directions, instructions) = parse(TEST_INPUT_3);
+        assert_eq!(part_2(&directions, &instructions), 6);
     }
 }
